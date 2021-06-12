fde6ef50529eec6bf6e54617d104244715617c1f
Diego-David <85714629+Diego-David@users.noreply.github.com>
2021-06-12 00:03:37 -0500

    Delete applications directory

--- applications/departamento/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class NewDepartamentoView(forms.Form):
-    nombre = forms.CharField(max_length=50)
-    apellidos = forms.CharField(max_length=50, required=True)
-    departamento = forms.CharField(max_length=50, required=True)
-    shorname = forms.CharField(max_length=20, required=True)
