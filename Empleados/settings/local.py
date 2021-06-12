a1cdde95b42f5cefba3819cce2259acd4894fa0d
DiegoDavid <diego.gavilanes8343@utc.edu.ec>
2021-06-12 00:07:03 -0500

    otro proyectito

---
+++ Empleados/settings/local.py
@@ -0,0 +1,39 @@
+from . base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',   
+        'USER': 'diego',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
---
+++ applications/departamento/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+from django.views.generic.edit import FormView
+from django.views.generic.list import ListView  # => con esto heredamos un form para ingresar datos en dos o más modelos
+
+from .forms import NewDepartamentoView
+
+from applications.persona.models import Empleado # ====> AQUI estoy recuperando los datos del modelo Empleado
+from .models import Departamento
+
+class DepartamentoListView (ListView):
+    model = Departamento
+    template_name = "departamento/lista.html"
+    context_object_name = 'departamentos'
+
+class NewDepartamentoView(FormView):
+    template_name = 'departamento/formdoblemodel.html'
+    form_class = NewDepartamentoView
+    success_url = '/'
+
+    def form_valid(self, form):
+        print("===============EEEO")
+        #===========================================================
+        # instanticia del modelo departamento por laclave foranea
+        depa = Departamento(
+            name = form.cleaned_data['departamento'],
+            shor_name = form.cleaned_data['shorname']
+        )
+        depa.save()
+        #===========================================================
+        nombre = form.cleaned_data['nombre'] # ====> AQUI estoy recuperando los datos del modelo Empleado
+        apellido = form.cleaned_data['apellidos'] # ====> AQUI estoy recuperando los datos del modelo Empleado
+        Empleado.objects.create(
+            first_name = nombre,
+            last_name = apellido,
+            job = '1',
+            departamento = depa
+
+
+        )
+        return super(NewDepartamentoView, self).form_valid(form)
+
+
+
+# Create your views here.
